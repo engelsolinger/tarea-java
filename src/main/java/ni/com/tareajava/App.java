3bb203414fd85614b5b81678c20d6220c156c939
PcLocalOlinger <engelsolinger@gmail.com>
2017-11-04 15:45:36 -0600

    Creación de la clase para revertir un arreglo
    se implemento una clase para revertir y presentar el resultado de la reversion de un arreglo

--- src/main/java/ni/com/tareajava/App.java
+++ src/main/java/ni/com/tareajava/App.java
@@ -6,8 +6,28 @@ package ni.com.tareajava;
  */
 public class App 
 {
+    private String[] iniciales = {"O","E","P","M"};
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App cApp = new App();
+
+        cApp.revertirAregglo();
+
+        //System.out.println( "Hello World!" );
+    }
+
+    private void revertirAregglo(){
+       String[] nuevoIniciales= new String[this.iniciales.length];
+       Integer conteo = 0;
+
+        for (int x=iniciales.length-1;x>=0;x--){
+            nuevoIniciales[conteo]=iniciales[x];
+            conteo++;
+        }
+
+        for(int x=0;x<=nuevoIniciales.length-1;x++){
+            System.out.println(nuevoIniciales[x]);
+        }
+
     }
 }
